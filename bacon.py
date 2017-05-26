ef557af6bc45d63664e6690a97e838de162ff7b1
wintershammer <wintershammer@gmail.com>
2017-05-26 17:27:03 +0300

    Updated bacon.py
    Updated bacon.py with my solution to the assignment's task

--- bacon.py
+++ bacon.py
@@ -1,24 +1,87 @@
-
 from urllib.request import urlopen,Request
 from urllib.parse import urlencode
+import json
+import sys
 
-#import json	# κρατήστε αυτό αν χειρίζεστε αποτελέσματα σε μορφή json...
-#import xml.etree.ElementTree as ET	# ...ή αυτό για αποτελέσματα σε xml
+endpoint = "http://data.linkedmdb.org/sparql?"
+print("Gimme a name:")
+kevin = input()
 
+getActorsByName = """
+PREFIX movie: <http://data.linkedmdb.org/resource/movie/>
+PREFIX lmdba: <http://data.linkedmdb.org/resource/actor/>
+PREFIX lmdbf: <http://data.linkedmdb.org/resource/film/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+SELECT ?actor ?aname WHERE {
+?actor a movie:actor .
+?actor movie:actor_name ?aname.
+FILTER (regex(?aname,"^""" + kevin + """","i"))
+}
+"""
 
-# ζητήστε ένα μικρό όνομα από τον χρήστη
+params = { 'query': getActorsByName }
+paramstr = urlencode(params)
 
+req = Request(endpoint+paramstr)
+req.add_header('Accept','application/sparql-results+json')
+page = urlopen(req)
+text = page.read().decode('utf-8')
+page.close()
 
-# με την πρώτη ερώτηση SPARQL ανακτήστε λίστα ηθοποιών (και τα URIs τους) με το όνομα αυτό
+toJson = json.loads(text)
 
+actorsDict = {}
 
-# ζητήστε από τον χρήστη να επιλέξει ηθοποιό
+# iterate over results
+for binding in toJson['results']['bindings']:
+        actorsDict[binding['aname']['value']] = binding['actor']['value']
 
+if not actorsDict:
+	print("No actors found")
+	sys.exit(0)
 
-# με τη δεύτερη ερώτηση SPARQL βρείτε τους συμπρωταγωνιστές/ταινίες
+print("Here's a list of actors you can choose from: ")
+for key in actorsDict:
+        print(key)
 
+print("Choose an actor: ")
+specificActor = input()
 
-# τυπώστε ονόματα και τίτλους ταινιών
+if (specificActor in actorsDict):
+	selectedURI = actorsDict[specificActor]
+else:
+	print("No such actor")
+	sys.exit(0)
 
+getCoActors = """
+PREFIX movie: <http://data.linkedmdb.org/resource/movie/>
+PREFIX lmdba: <http://data.linkedmdb.org/resource/actor/>
+PREFIX lmdbf: <http://data.linkedmdb.org/resource/film/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
+SELECT DISTINCT(?filmName) ?actorName WHERE {
+	?film movie:actor <""" + selectedURI+"""> .
+	?film movie:actor ?oneDeg .
+	?film rdfs:label ?filmName .
+	?oneDeg movie:actor_name ?actorName .
+	FILTER(?oneDeg != <""" + selectedURI+""">)
+}
+
+"""
+
+params = { 'query': getCoActors }
+paramstr = urlencode(params)
+
+req = Request(endpoint+paramstr)
+req.add_header('Accept','application/sparql-results+json')
+page = urlopen(req)
+text = page.read().decode('utf-8')
+page.close()
+
+toJsonCoActors = json.loads(text)
+
+print("Here's a list of actors and the films they starred along with " + specificActor + " in: ")
+for binding in toJsonCoActors['results']['bindings']:
+       print(binding['actorName']['value'],"in", binding['.0']['value'])
 
